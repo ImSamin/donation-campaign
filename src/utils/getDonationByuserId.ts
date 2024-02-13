b3c669f8cfcb8c8bda14bf40dc17ac5080df57b1
ImSamin <im.samin@gmail.com>
2024-02-13 23:25:40 +0600

    bug-fixed

--- src/utils/getDonationByuserId.ts
+++ src/utils/getDonationByuserId.ts
@@ -6,7 +6,9 @@ export const getDonationByUserId = async () => {
   const res = await fetch(
     `https://donation-campaign-backend.vercel.app/api/v1/donation/?id=${session?.id}`,
     {
-      cache: "no-store",
+      next: {
+        revalidate: 5,
+      },
     }
   );
 
